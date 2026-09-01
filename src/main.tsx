666192400d6dfe404eccb7159679409adb170804
Byron <Garciabyron175@gmail.com>
2026-08-31 19:20:23 -0700

    Refactor app structure to integrate React Query and update routing; create AppProviders and queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 30_000,
-      refetchOnWindowFocus: false,
-    },
-  },
-})
+import { AppProviders } from './app/providers'
+import { router } from './app/router'
+
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <AppProviders>
+      <RouterProvider router={router} />
+    </AppProviders>
   </StrictMode>,
 )
